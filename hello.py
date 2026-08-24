087837c00ebf61d2e13204a72f947611212fc430
nikia nik <nikianikamallarijani@gmail.com>
2026-08-24 19:14:01 +0430

    saving my progress once again

--- hello.py
+++ hello.py
@@ -10,26 +10,37 @@ questions: list = [
     ('question4', ['A' , 'B' , 'C' , 'D'],'B'),
     ('question5', ['A' , 'B' , 'C' , 'D'],'C')
 ]
-print(len(questions))
+
 
 def show_menu():
-print('1. start')
-print('2. exit')
-choice = input('choose option 1 or 2').strip()
-    if choice =='1':
-        score =0
-        for q, options, correct_answer in questions:
-            print(f'\n{q}')
-            for opt in options:
-                print(opt)
-            answer: str = input('your answer: ').strip().upper()
-            if answer==correct_answer:
-                print('correct')
-                score+=1
-            else:
-                print('wrong')
-                score-=3 
+    print('1. start')
+    print('2. exit')
+    choice = input('choose option 1 or 2').strip()
+    return choice
+
+def playing():
+    score =0
+    for q, options, correct_answer in questions:
+        print(f'\n{q}')
+        for opt in options:
+            print(opt)
+        answer: str = input('your answer: ').strip().upper()
+        if answer==correct_answer:
+            print('correct')
+            score+=1
+        else:
+            print('wrong')
+            score-=3 
+    return score
+        
+
+def show_result(score):
         print(f'{(score)/len(questions)*100}%')
-    elif choice =='2':
-        print('goodbye')
-        break
+    
+choice=show_menu()
+if choice=='1':
+    score=playing()
+    show_result(score)
+
+elif choice=='2':
+    print('goodbye')
